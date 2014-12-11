8ed54d91ca3848c9eab844413ab86d6c9a86b15d
men1n2 <amine1n2@gmail.com>
2014-12-11 05:11:30 +0100

    Custom filter to show suggestions without concerns on majus/min, "chakl" on arabic characters, etc...

--- app/src/main/java/com/men1n2/couca/enterText.java
+++ app/src/main/java/com/men1n2/couca/enterText.java
@@ -19,9 +19,11 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -29,6 +31,7 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -70,6 +73,8 @@ public class enterText extends Activity {
 
         setContentView(R.layout.activity_enter_text);
 
+        readNameListFromTextFile();
+
         // Dim System Bar
         // This example uses decor view, but you can use any visible view.
         View decorView = getWindow().getDecorView();
@@ -102,14 +107,6 @@ public class enterText extends Activity {
         manager.addView(view, localLayoutParams);
         // ------------------------
 
-        Context context = this;
-        Resources res = context.getResources();
-        // Change locale settings in the app.
-        DisplayMetrics dm = res.getDisplayMetrics();
-        android.content.res.Configuration conf = res.getConfiguration();
-        conf.locale = new Locale("en", "US");
-        res.updateConfiguration(conf, dm);
-
         enterTextVideo = (VideoView) findViewById(R.id.videoView2);
         enterTextVideo.setVideoPath("android.resource://" + getPackageName() + "/" + R.drawable.entertextvid);
         enterTextVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -131,7 +128,6 @@ public class enterText extends Activity {
         // Timer cycle time
         timer.schedule(timerTask, 45000);
 
-
         // Show keyboard automatically on focus
         editField = (AutoCompleteTextView) findViewById(R.id.personNameEditText);
         if (editField != null) {
@@ -148,31 +144,99 @@ public class enterText extends Activity {
         }
 
         // Populate the text field with names to autocomplete with
-        readNameListFromTextFile();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, listOfNames);
+                android.R.layout.simple_dropdown_item_1line, listOfNames) {
+            public ArrayList<String> suggestions = new ArrayList<String>();
+            
+            @Override
+            public Filter getFilter() {
+                return nameFilter;
+            }
+
+            Filter nameFilter = new Filter() {
+                @Override
+                public String convertResultToString(Object resultValue) {
+                    String str = (String) resultValue;
+                    return str;
+                }
+                @Override
+                protected FilterResults performFiltering(CharSequence charSequence) {
+                    System.out.println("#### Filterin999...");
+                    suggestions.clear();
+                    readNameListFromTextFile();
+                    if(charSequence != null) {
+                        int index = 0;
+                        String testString = "";
+                        for (String s : listOfNames) {
+                            testString = deleteUmlauts(s);
+                            if (testString.length() >= charSequence.length()){
+                                /*if (charSequence.toString().toLowerCase().contains(testString.substring(0, charSequence.length()-1).toLowerCase())) {
+                                    System.out.println("#### Tghueeee");
+                                    System.out.println("#### To test : " + listOfNames.get(index));
+                                    System.out.println("### Its substr = " + testString.substring(0, charSequence.length()));
+                                    System.out.println("#### Suggest : " + charSequence);
+                                    suggestions.add(listOfNames.get(index));
+                                }*/
+                                int counter = 0;
+                                for(int j = 0; j < charSequence.length(); j++) {
+                                    System.out.println("### Comparing : " + charSequence.toString().charAt(j) + " || " + testString.charAt(j));
+                                    if(charSequence.toString().toLowerCase().charAt(j) == testString.toLowerCase().charAt(j)) counter++;
+                                }
+                                if(counter == charSequence.length()) suggestions.add(listOfNames.get(index));
+                            }
+                            index++;
+                        }
+                        FilterResults filterResults = new FilterResults();
+                        filterResults.values = suggestions;
+                        filterResults.count = suggestions.size();
+                        System.out.println("#### N of suggs = " + suggestions.size());
+                        return filterResults;
+                    } else {
+                        return new FilterResults();
+                    }
+                }
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    List<String> filteredList = (List<String>) results.values;
+                    if(results != null && results.count > 0) {
+                        clear();
+                        for (String c : filteredList) {
+                            add(c);
+                        }
+                        notifyDataSetChanged();
+                    }
+                }
+            };
+        };
         editField.setThreshold(1);
         editField.setAdapter(adapter);
         editField.addTextChangedListener(new TextWatcher() {
             public void afterTextChanged(Editable s) {
-                timer.cancel();
-                // Create Timer
-                timer = new Timer();
-                // Add timer task
-                TimerTask timerTask = new TimerTask() {
+                // This to run the code on the main UI thread and avoid exception
+                runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        restartApp();
+                        timer.cancel();
+                        // Create Timer
+                        timer = new Timer();
+                        // Add timer task
+                        TimerTask timerTask = new TimerTask() {
+                            @Override
+                            public void run() {
+                                restartApp();
+                            }
+                        };
+                        // Timer cycle time
+                        timer.schedule(timerTask, 20000);
                     }
-                };
-                // Timer cycle time
-                timer.schedule(timerTask, 30000);
+                });
             }
 
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
 
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
         });
 
@@ -256,7 +320,7 @@ public class enterText extends Activity {
         confirmButton.setBackgroundResource(R.drawable.confirmbuttonpressed);
         EditText inputField = (EditText) findViewById(R.id.personNameEditText);
         // Test if the word is bad or no
-        if (testBadWords(inputField.getText().toString())) {
+        if (!testPermittedWords(inputField.getText().toString())) {
             // Show Alert if the word is bad word
             inputField.setText("");
             AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
@@ -333,22 +397,10 @@ public class enterText extends Activity {
     }
 
     /**
-     * To test if the word is a bad word or no
+     * To test if the word entered is on the list or no
      */
-    public static boolean testBadWords(String word) {
-        String[] badWordsArray = {"3asba", "aasba", "asba", "zeb ", "zab ", "zabb", "zebb", "zabour", "zabbour", "ta7an", "tahan",
-                "ta77an", "9a7ba", "ka7ba", "kahba", "9ahba", "miboun", "mibouna", "3atay", "3attay", "aattay", "taffar", "tafar", "mag7out",
-                "mak7out", "mnayek ", "nayek", "nik", "nike", "niq", "nique", "bachoula", "katla", "catla", "za3ka", "zaaka", "sorm", "terma",
-                "nikommek", "nik omek", "nik ommek", "nik o5tek", "nik okhtek", "niko5etk", "nik bouk ", "zabbourommek", "zabbouromek",
-                "zabourommek", "zabouromek", "zok", "zock", "zokk", "zokkommek", "zokommek", "zokomek", "zokkomek", "rabbek", "rabek", "rabb",
-                "rabomek", "rabbommek", "rabomek", "rabbomek", "rabommek", "dirrabbek", "dirabek", "dirrabek", "dirabbek", "dinrabek",
-                "dinrabbek", "pute", "bitch", "fuck", "pd", "flobb ", "flob", "chlambout", "torch9an", "nouna", "batrouna", "batroun", "zebi",
-                "zebbi", "nam", "nami", "namm", "bachla", "zmonka", "terma", "tartour", "bajbouj", "wral", "marzouki", "3asbet",
-                "عصبة", "زب", "زبور", "زبر", "طحان", "طحين", "قحبة", "قحب", "ميبون", "ميبونة", "عطاي", "طفار", "مكحوط", "منيك", "نيك", "نياك",
-                "بشلة", "بشولة", "كتلة", "زعكة", "زعك", "صرم", "ترمة", "نيك أمك", "نيكأمك", "نيك أختك", "نيكأختك", "نيك بوك", "زبور أمك", "زك",
-                "زك أمك", "زكأمك", "ربك", "رب", "رب أمك", "ديربك", "دربك", "دينربك", "دنربك", "فلب", "شلمبوت", "شلمبوط", "طرشقان", "نونة", "بترون",
-                "بترونة", "بطرونة", "بطرون", "زبي", "نم", "نمي", "زمنكة", "المرزوقي", "الورل", "البجبوج", "الطرطور", "العصبات"};
-        for (String s : badWordsArray) {
+    private boolean testPermittedWords(String word) {
+        for (String s : listOfNames) {
             int i = s.indexOf(word);
             if (i >= 0) {
                 // found a match to "word" at offset i
@@ -361,7 +413,11 @@ public class enterText extends Activity {
     /**
      * To populate the names' array from text file on the SD card
      */
-    private void readNameListFromTextFile() {
+    public void readNameListFromTextFile() {
+        // Path to test on emulator/device without SD card
+        /*String fpath = File.separator + "mnt" + File.separator + "sdcard" + File.separator
+                    + "Tarbijet" + File.separator + "listOfTarbijet.csv";*/
+        // Path in a device with SD card
         String fpath = File.separator + "storage" + File.separator + "sdcard1" + File.separator
                 + "Tarbijet" + File.separator + "listOfTarbijet.csv";
         File file = null;
@@ -395,4 +451,82 @@ public class enterText extends Activity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    /**
+     * Normalize Method
+     * \p{Mn} in regex (called Diphthongs)
+     *
+     * @return String
+     */
+    public String deleteUmlauts(String input) {
+
+        //Remove honorific sign
+        input = input.replaceAll("\u0610", "");//ARABIC SIGN SALLALLAHOU ALAYHE WA SALLAM
+        input = input.replaceAll("\u0611", "");//ARABIC SIGN ALAYHE ASSALLAM
+        input = input.replaceAll("\u0612", "");//ARABIC SIGN RAHMATULLAH ALAYHE
+        input = input.replaceAll("\u0613", "");//ARABIC SIGN RADI ALLAHOU ANHU
+        input = input.replaceAll("\u0614", "");//ARABIC SIGN TAKHALLUS
+
+        //Remove koranic anotation
+        input = input.replaceAll("\u0615", "");//ARABIC SMALL HIGH TAH
+        input = input.replaceAll("\u0616", "");//ARABIC SMALL HIGH LIGATURE ALEF WITH LAM WITH YEH
+        input = input.replaceAll("\u0617", "");//ARABIC SMALL HIGH ZAIN
+        input = input.replaceAll("\u0618", "");//ARABIC SMALL FATHA
+        input = input.replaceAll("\u0619", "");//ARABIC SMALL DAMMA
+        input = input.replaceAll("\u061A", "");//ARABIC SMALL KASRA
+        input = input.replaceAll("\u06D6", "");//ARABIC SMALL HIGH LIGATURE SAD WITH LAM WITH ALEF MAKSURA
+        input = input.replaceAll("\u06D7", "");//ARABIC SMALL HIGH LIGATURE QAF WITH LAM WITH ALEF MAKSURA
+        input = input.replaceAll("\u06D8", "");//ARABIC SMALL HIGH MEEM INITIAL FORM
+        input = input.replaceAll("\u06D9", "");//ARABIC SMALL HIGH LAM ALEF
+        input = input.replaceAll("\u06DA", "");//ARABIC SMALL HIGH JEEM
+        input = input.replaceAll("\u06DB", "");//ARABIC SMALL HIGH THREE DOTS
+        input = input.replaceAll("\u06DC", "");//ARABIC SMALL HIGH SEEN
+        input = input.replaceAll("\u06DD", "");//ARABIC END OF AYAH
+        input = input.replaceAll("\u06DE", "");//ARABIC START OF RUB EL HIZB
+        input = input.replaceAll("\u06DF", "");//ARABIC SMALL HIGH ROUNDED ZERO
+        input = input.replaceAll("\u06E0", "");//ARABIC SMALL HIGH UPRIGHT RECTANGULAR ZERO
+        input = input.replaceAll("\u06E1", "");//ARABIC SMALL HIGH DOTLESS HEAD OF KHAH
+        input = input.replaceAll("\u06E2", "");//ARABIC SMALL HIGH MEEM ISOLATED FORM
+        input = input.replaceAll("\u06E3", "");//ARABIC SMALL LOW SEEN
+        input = input.replaceAll("\u06E4", "");//ARABIC SMALL HIGH MADDA
+        input = input.replaceAll("\u06E5", "");//ARABIC SMALL WAW
+        input = input.replaceAll("\u06E6", "");//ARABIC SMALL YEH
+        input = input.replaceAll("\u06E7", "");//ARABIC SMALL HIGH YEH
+        input = input.replaceAll("\u06E8", "");//ARABIC SMALL HIGH NOON
+        input = input.replaceAll("\u06E9", "");//ARABIC PLACE OF SAJDAH
+        input = input.replaceAll("\u06EA", "");//ARABIC EMPTY CENTRE LOW STOP
+        input = input.replaceAll("\u06EB", "");//ARABIC EMPTY CENTRE HIGH STOP
+        input = input.replaceAll("\u06EC", "");//ARABIC ROUNDED HIGH STOP WITH FILLED CENTRE
+        input = input.replaceAll("\u06ED", "");//ARABIC SMALL LOW MEEM
+
+        //Remove tatweel
+        input = input.replaceAll("\u0640", "");
+
+        //Remove tashkeel
+        input = input.replaceAll("\u064B", "");//ARABIC FATHATAN
+        input = input.replaceAll("\u064C", "");//ARABIC DAMMATAN
+        input = input.replaceAll("\u064D", "");//ARABIC KASRATAN
+        input = input.replaceAll("\u064E", "");//ARABIC FATHA
+        input = input.replaceAll("\u064F", "");//ARABIC DAMMA
+        input = input.replaceAll("\u0650", "");//ARABIC KASRA
+        input = input.replaceAll("\u0651", "");//ARABIC SHADDA
+        input = input.replaceAll("\u0652", "");//ARABIC SUKUN
+        input = input.replaceAll("\u0653", "");//ARABIC MADDAH ABOVE
+        input = input.replaceAll("\u0654", "");//ARABIC HAMZA ABOVE
+        input = input.replaceAll("\u0655", "");//ARABIC HAMZA BELOW
+        input = input.replaceAll("\u0656", "");//ARABIC SUBSCRIPT ALEF
+        input = input.replaceAll("\u0657", "");//ARABIC INVERTED DAMMA
+        input = input.replaceAll("\u0658", "");//ARABIC MARK NOON GHUNNA
+        input = input.replaceAll("\u0659", "");//ARABIC ZWARAKAY
+        input = input.replaceAll("\u065A", "");//ARABIC VOWEL SIGN SMALL V ABOVE
+        input = input.replaceAll("\u065B", "");//ARABIC VOWEL SIGN INVERTED SMALL V ABOVE
+        input = input.replaceAll("\u065C", "");//ARABIC VOWEL SIGN DOT BELOW
+        input = input.replaceAll("\u065D", "");//ARABIC REVERSED DAMMA
+        input = input.replaceAll("\u065E", "");//ARABIC FATHA WITH TWO DOTS
+        input = input.replaceAll("\u065F", "");//ARABIC WAVY HAMZA BELOW
+        input = input.replaceAll("\u0670", "");//ARABIC LETTER SUPERSCRIPT ALEF
+
+        return input;
+    }
 }
+
